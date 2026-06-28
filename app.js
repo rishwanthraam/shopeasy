5644a59be3617d54ae0bf177a3ba974796c97381
rishwanthraam <rishwanthraam392006@gmail.com>
2026-06-28 11:03:27 +0530

    app.js Modify: message has changed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Welcome to ShopEasy!");
+    res.send("Welcome to ShopEasy! from Rishwanth ");
 });
 
 app.listen(PORT, () => {
